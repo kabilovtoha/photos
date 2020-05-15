3cc7c24f0a6bbacdb04f3e832dbe3bfa303a10ac
kabilovtoha <kabilov2011@gmail.com>
2020-05-15 06:59:42 +0400

    fix

--- geophoto/urls.py
+++ geophoto/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('geo.urls', namespace='geo')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # noqa
